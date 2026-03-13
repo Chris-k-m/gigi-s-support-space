5341c0a6387f726bb95d90d99fe487c5d81a10db
sipsmarthub-Kenya <sipsmarthub@gmail.com>
2026-03-13 23:41:48 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,22 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
+    server: {
+        host: "::",
+        port: 8080,
+        hmr: {
+            overlay: false,
+        },
     },
-  },
-    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-    base: "/",
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+    base: "/gigi-s-support-space/",
+
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 }));
